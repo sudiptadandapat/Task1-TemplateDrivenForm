3a860644d8483c0e3f52a35c4521707bed85a1ab
unknown <sd2036@cse.jgec.ac.in>
2020-11-09 14:26:06 +0530

    template driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'form1';
+
+  @ViewChild('f') signupForm: NgForm;
+  genders =['Male','Female']
+
+  submitted=false;
+  entryArray=[];
+
+  user={
+    name:'',
+    email:'',
+    gender:'',
+    phoneNo:0
+    
+  }
+
+  onSubmit(){
+    this.submitted=true;
+    this.user.name=this.signupForm.value.name;
+    this.user.email=this.signupForm.value.email;
+    this.user.gender=this.signupForm.value.gender;
+    this.user.phoneNo=this.signupForm.value.phoneNo;
+    console.log(this.user);
+    this.entryArray.push(this.user);
+    
+  }
+
+  onReset(){
+    this.signupForm.reset();
+  }
 }
